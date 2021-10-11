24797cc8b482ccc1e08908a526b90424d7c70158
LeandroCieriS <leandro.cieri.s@gmail.com>
2021-10-11 15:17:47 +0100

    Refactor: tests

--- Gilded-rose.Test/GildedRoseShould.cs
+++ Gilded-rose.Test/GildedRoseShould.cs
@@ -7,62 +7,32 @@ namespace Gilded_rose.Test
 {
     public class GildedRoseShould
     {
-
-        [Test]
-        public void decrease_quality_of_items()
+        [TestCase(10, 20, 19)]
+        [TestCase(-4, 25, 23)]
+        [TestCase(10, 0, 0)]
+        public void decrease_quality_of_items(int sellIn, int quality, int expected)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Normal item", SellIn = 10, Quality = 20 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Normal item", SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(Items);
 
             app.UpdateQuality();
 
-            Items.First().Quality.Should().Be(19);
+            Items.First().Quality.Should().Be(expected);
         }
 
-        [Test]
-        public void decrease_quality_of_item_faster_when_expired()
+
+        [TestCase(1, 20, 21)]
+        [TestCase(0, 20, 22)]
+        public void increase_quality_of_aged_brie(int sellIn, int quality, int expected)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Normal item", SellIn = 0, Quality = 25 } };
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(Items);
 
             app.UpdateQuality();
 
-            Items.First().Quality.Should().Be(23);
+            Items.First().Quality.Should().Be(expected);
         }
 
-        [Test]
-        public void not_decrease_quality_of_item_when_it_is_zero()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "Normal item", SellIn = 10, Quality = 0 } };
-            var app = new GildedRose(Items);
-
-            app.UpdateQuality();
-
-            Items.First().Quality.Should().Be(0);
-        }
-
-        [Test]
-        public void increase_quality_of_aged_brie()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 20 } };
-            var app = new GildedRose(Items);
-
-            app.UpdateQuality();
-
-            Items.First().Quality.Should().Be(21);
-        }  
-        
-        [Test]
-        public void increase_quality_of_aged_brie_faster_when_expired()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 20 } };
-            var app = new GildedRose(Items);
-
-            app.UpdateQuality();
-
-            Items.First().Quality.Should().Be(22);
-        }
-        
         [Test]
         public void should_not_increase_quality_of_item_above_fifty()
         {
@@ -85,49 +55,19 @@ namespace Gilded_rose.Test
             Items.First().Quality.Should().Be(80);
         }
 
-        [Test]
-        public void should_increase_quality_of_backstage()
+        [TestCase(11, 30, 31)]
+        [TestCase(10, 30, 32)]
+        [TestCase(5, 30, 33)]
+        [TestCase(0, 30, 0)]
+        public void should_increase_quality_of_backstage(int sellIn, int quality, int expected)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 60, Quality = 30 } };
+            IList<Item> Items = new List<Item> { new Item 
+                { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = sellIn, Quality = quality } };
             var app = new GildedRose(Items);
 
             app.UpdateQuality();
 
-            Items.First().Quality.Should().Be(31);
+            Items.First().Quality.Should().Be(expected);
         }
-
-        [Test]
-        public void should_increase_quality_of_backstage_twice_as_fast_when_SellIn_between_ten_and_five()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 30 } };
-            var app = new GildedRose(Items);
-
-            app.UpdateQuality();
-
-            Items.First().Quality.Should().Be(32);
-        }
-
-        [Test]
-        public void should_increase_quality_of_backstage_thrice_as_fast_when_SellIn_between_five_and_zero()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 30 } };
-            var app = new GildedRose(Items);
-
-            app.UpdateQuality();
-
-            Items.First().Quality.Should().Be(33);
-        }
-
-        [Test]
-        public void should_drop_quality_to_zero_when_backstage_item_expires()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 30 } };
-            var app = new GildedRose(Items);
-
-            app.UpdateQuality();
-
-            Items.First().Quality.Should().Be(0);
-        }
-
     }
 }
