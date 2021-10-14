13368a339ab4d718930a600274f39c3727f5073e
LeandroCieriS <leandro.cieri.s@gmail.com>
2021-10-14 10:42:05 +0100

    refactor: factory items

--- Gilded-rose/GildedRose.cs
+++ Gilded-rose/GildedRose.cs
@@ -1,104 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Gilded_rose
 {
     public class GildedRose
     {
         public IList<Item> Items;
+        private ItemFactory itemFactory = new ItemFactory();
 
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
         }
-
-        private bool IsNormalItem(Item item)
-        {
-            return item.Name.Equals("Normal item");
-        }
-
-        private bool IsLegendaryItem(Item item)
-        {
-            return item.Name.Equals("Sulfuras, Hand of Ragnaros");
-        }
-
-        private bool IsIncreasingItem(Item item)
-        {
-            return item.Name.Equals("Aged Brie") || item.Name.Equals("Backstage passes to a TAFKAL80ETC concert");
-        }
+        
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var item in Items)
             {
-                if (IsNormalItem(Items[i]))
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
+                itemFactory.CreateItem(item.Name).UpdateQuality(item);
             }
         }
-
     }
 
     public class Item
---
+++ Gilded-rose/ItemFactory.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace Gilded_rose
+{
+    internal class ItemFactory
+    {
+        private Dictionary<string, Func<IItem>> _dictionary;
+        
+        public ItemFactory()
+        {
+            _dictionary = new Dictionary<string, Func<IItem>>
+            {
+                {
+                    "Aged Brie", () => new AgedBrieItem()
+                },
+                {
+                    "Backstage passes to a TAFKAL80ETC concert", () => new BackstageItem()
+                },
+                {
+                    "Sulfuras, Hand of Ragnaros", () => new SulfurasItem()
+                }
+                //},
+                //{
+                //    "Conjured", () => new ConjuredItem()
+                //}
+            };
+        }
+
+        public IItem CreateItem(string itemName)
+        {
+            if (_dictionary.ContainsKey(itemName))
+                return _dictionary[itemName].Invoke();
+
+            return new NormalItem();
+        }
+    }
+
+    internal interface IItem
+    {
+        void UpdateQuality(Item item);
+    }
+
+    internal class SulfurasItem : IItem
+    {
+        public void UpdateQuality(Item item)
+        {}
+    }
+
+    internal class AgedBrieItem : IItem
+    {
+        public void UpdateQuality(Item item)
+        {
+            item.SellIn--;
+
+            if (item.SellIn >= 0 && item.Quality < 50)
+            {
+                item.Quality++;
+            }
+            else if (item.SellIn < 0)
+            {
+                item.Quality = Math.Min(item.Quality += 2, 50);
+            }
+        }
+    }
+
+    internal class NormalItem : IItem
+    {
+        public void UpdateQuality(Item item)
+        {
+            item.SellIn--;
+
+            if (item.SellIn > 0 && item.Quality > 0)
+            {
+                item.Quality--;
+            }
+            else if (item.SellIn < 0 && item.Quality > 0)
+            {
+                item.Quality -= 2;
+            }
+        }
+    }
+
+    internal class BackstageItem : IItem
+    {
+        public void UpdateQuality(Item item)
+        {
+            item.SellIn--;
+
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0;
+            }
+            else
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+
+                if (item.SellIn < 10 && item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+
+                if (item.SellIn < 5 && item.Quality < 50)
+                {
+                    item.Quality++;
+                }
+            }
+        }
+    }
+}
